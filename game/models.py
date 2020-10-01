707b45e3b830fa8b10a1d1294c9eb6388a63b9aa
floralyellow <ceci@DESKTOP-N4FE6VK.localdomain>
2020-10-01 15:18:47 +0200

    create multiple models

--- game/models.py
+++ game/models.py
@@ -11,3 +11,32 @@ class User(models.Model):
  
     def _str_(self):
         return self.title
+
+
+class Session(models.Model):
+    id = int
+    creationDate = datetime.date
+    id_user = int
+    nb_room = int
+    name = models.TextField()
+
+
+class Room(models.Model):
+    id = int
+    id_session = int
+    order = int
+    is_special = bool
+    is_done = bool
+
+
+class RoomMonsters(models.Model):
+    id = int
+    id_room = int
+    id_monster = int
+
+
+class Monster(models.Model):
+    id = int
+    name = models.TextField
+    life_point = int
+    damage = int
