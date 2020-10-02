5abdb74ddfd8fd4b9ebacb244a8cd959f7fff71a
floralyellow <ceci@DESKTOP-N4FE6VK.localdomain>
2020-10-02 21:26:19 +0200

    add database connection

--- back/settings.py
+++ back/settings.py
@@ -74,14 +74,17 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'game',
+        'USER': 'ceci',
+        'PASSWORD': 'ceci',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
